c060883ec503b27da449dbebf152186157d0bce6
jackofsporks <jackofsporks@users.noreply.github.com>
2017-12-25 15:53:11 -0700

    12/22/17 stream triggering but not working

---
+++ spec/ide-fridge-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IdeFridgeView from '../lib/ide-fridge-view';
+
+describe('IdeFridgeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
